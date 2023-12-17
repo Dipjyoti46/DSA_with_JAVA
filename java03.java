1c75e413ceabe763072626c30d73c160857ba2ba
dipjyoti46 <dipjyotisaikia46@gmail.com>
2023-12-17 21:54:35 +0530

    first commit

---
+++ java03.java
@@ -0,0 +1,10 @@
+public class java03{
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            System.out.printf("Hello World! %d\n", i);
+            i++;
+        }
+    }
+
+}
