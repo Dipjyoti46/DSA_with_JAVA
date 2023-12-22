d19ac4ead0a8714a5b37b86cd9430e73aa8011cb
dipjyoti46 <dipjyotisaikia46@gmail.com>
2023-12-22 20:29:24 +0530

    commit

--- dsawithjava/java02.java
+++ dsawithjava/java02.java
@@ -1,6 +1,20 @@
 package dsawithjava;
+import java.util.Scanner;
 
 public class java02 {
-    
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int num,rem,rev_num;
+        System.out.println("Enter the number:");
+        num=sc.nextInt();
+        rev_num=0;
+        
+        while(num!=0){
+            rem=num%10;
+            rev_num=rev_num*10+rem;
+            num=num/10;
+        }
+        System.out.println("the reversed number is "+rev_num);
+    }
 
 }
