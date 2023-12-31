261e613418d450feadb95aeff34fed39790e64a8
dipjyoti46 <dipjyotisaikia46@gmail.com>
2023-12-31 10:00:08 +0530

    commit

--- dsawithjava/searching/search_in_2d.java
+++ dsawithjava/searching/search_in_2d.java
@@ -1,17 +1,27 @@
 package searching;
+import java.util.Arrays;
 import java.util.Scanner;
 public class search_in_2d {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
 
-        int[][] arr=new int[4][];
+        int[][] arr=new int[4][4];
         System.out.println("Enter the elements of the array: ");
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 arr[i][j]=in.nextInt();
             }
         }
-
+        System.out.println("The array is :");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(arr[i][j]+" ");        
+            }
+            System.out.println();
+        }
+        System.out.println("Enter the target:");
+        int target=in.nextInt();
+        search2D(arr,target);
     }
     static void search2D(int[][] arr, int target){
         for (int i = 0; i < arr.length; i++) {
